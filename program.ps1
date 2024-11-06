3fb6647db8d604c39f153a9285c51a595670e053
yousefhejazi <89594642+yousefhejazi@users.noreply.github.com>
2024-11-06 09:15:56 -0800

    Update program.ps1

--- program.ps1
+++ program.ps1
@@ -2,8 +2,6 @@ Here i will show you how to check or fetch for your publicIP adresss.
 
 to create a powershell code that retrieves our ip address we need to use a web api to make it easy, we put this command in ours windows powershell.
 
-function Get-PublicIP { try { $ip = Invoke-RestMethod -Uri "https://api.ipify.org?format=text" Write-Output "Your public IP address is: $ip" } catch { Write-Output "Error: Unable to retrieve public IP address. Please check your internet connection." } }
+ curl ifconfig.me	
 
-and after we done this we can use a much for simple command "Get-PublicIP" and it will show us our ip adress.
-
-and i solved this with help from Chatgbt.
+ if you done this correct there pops up a menu with your ip address like you can see in the readme file.
