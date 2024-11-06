d9ed517e14ab084b58751fa247dd44a212f2af1d
yousefhejazi <89594642+yousefhejazi@users.noreply.github.com>
2024-11-06 09:35:22 -0800

    Update program.ps1

--- program.ps1
+++ program.ps1
@@ -1,7 +1,3 @@
-Here i will show you how to check or fetch for your publicIP adresss.
-
-to create a powershell code that retrieves our ip address we need to use a web api to make it easy, we put this command in ours windows powershell.
+## Here i will show you how to check or fetch for your publicIP adresss.
 
  curl ifconfig.me	
-
- if you done this correct there pops up a menu with your ip address like you can see in the readme file.
